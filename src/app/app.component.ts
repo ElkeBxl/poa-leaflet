fb40463ae5517d1fb95b2a5ff4afe394f9489969
Elke Heymans <je.heymans@gmail.com>
2019-03-01 11:06:49 +0100

    Installed leaflet and set map to PortOfAntwerp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { latLng, tileLayer } from 'leaflet';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'poa-leaflet';
+	title = 'poa-leaflet';
+	options = {
+		layers: [
+			tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+		],
+		zoom: 12,
+		center: latLng(51.2756228, 4.3345864)
+	};
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		LeafletModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
